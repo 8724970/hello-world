df51a2a3aac70a323ad11e3c0514b0af39660c40
krijn_hoogendorp <krijn_hoogendorp@hotmail.com>
2014-11-07 16:36:38 +0100

    tweede scherm gemaakt

--- Hello8724970/gen/nl/mprog/setup/Hello8724970/R.java
+++ Hello8724970/gen/nl/mprog/setup/Hello8724970/R.java
@@ -1649,7 +1649,7 @@ containing a value of this type.
         public static final int action_mode_bar=0x7f050030;
         public static final int action_mode_bar_stub=0x7f05002f;
         public static final int action_mode_close_button=0x7f050022;
-        public static final int action_settings=0x7f050041;
+        public static final int action_settings=0x7f050042;
         public static final int activity_chooser_view_content=0x7f050023;
         public static final int always=0x7f05000d;
         public static final int beginning=0x7f050012;
@@ -1661,6 +1661,7 @@ containing a value of this type.
         public static final int dialog=0x7f050010;
         public static final int disableHome=0x7f05000a;
         public static final int dropdown=0x7f050011;
+        public static final int edit_message=0x7f050041;
         public static final int edit_query=0x7f050035;
         public static final int end=0x7f050014;
         public static final int expand_activities_button=0x7f050024;
@@ -1752,11 +1753,13 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030014;
         public static final int abc_search_view=0x7f030015;
         public static final int abc_simple_dropdown_hint=0x7f030016;
-        public static final int activity_main=0x7f030017;
-        public static final int support_simple_spinner_dropdown_item=0x7f030018;
+        public static final int activity_display_message=0x7f030017;
+        public static final int activity_main=0x7f030018;
+        public static final int support_simple_spinner_dropdown_item=0x7f030019;
     }
     public static final class menu {
-        public static final int main=0x7f0c0000;
+        public static final int display_message=0x7f0c0000;
+        public static final int main=0x7f0c0001;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1812,9 +1815,13 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000d;
-        public static final int action_settings=0x7f0a0011;
-        public static final int app_name=0x7f0a000f;
+        public static final int action_settings=0x7f0a0012;
+        public static final int app_name=0x7f0a0013;
+        public static final int button_send=0x7f0a0014;
+        public static final int edit_message=0x7f0a000f;
         public static final int hello_world=0x7f0a0010;
+        public static final int title_activity_display_message=0x7f0a0015;
+        public static final int title_activity_main=0x7f0a0011;
     }
     public static final class style {
         /** 
--- Hello8724970/src/nl/mprog/setup/Hello8724970/MainActivity.java
+++ Hello8724970/src/nl/mprog/setup/Hello8724970/MainActivity.java
@@ -1,12 +1,24 @@
+/** 
+ * name: Krijn Hoogendorp
+ * email: krijn_hoogendorp@hotmail.com
+ * HUID: 8724970
+ * 
+ * This program simply displays "Hello Android!" on an Android phone.
+ */
+
 package nl.mprog.setup.Hello8724970;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
+	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +44,15 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+    	Intent intent = new Intent(this, DisplayMessageActivity.class);
+    	EditText editText = (EditText) findViewById(R.id.edit_message);
+    	String message = editText.getText().toString();
+    	intent.putExtra(EXTRA_MESSAGE, message);
+    	startActivity(intent);
     }
 }
