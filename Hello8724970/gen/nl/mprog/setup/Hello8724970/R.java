03e4ba00744acf4e5cb9e952d347a223c4760291
krijn_hoogendorp <krijn_hoogendorp@hotmail.com>
2014-11-09 11:30:16 +0100

    added second page

--- Hello8724970/gen/nl/mprog/setup/Hello8724970/R.java
+++ Hello8724970/gen/nl/mprog/setup/Hello8724970/R.java
@@ -1754,12 +1754,14 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030015;
         public static final int abc_simple_dropdown_hint=0x7f030016;
         public static final int activity_display_message=0x7f030017;
-        public static final int activity_main=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int activity_display_next=0x7f030018;
+        public static final int activity_main=0x7f030019;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
     }
     public static final class menu {
         public static final int display_message=0x7f0c0000;
-        public static final int main=0x7f0c0001;
+        public static final int display_next=0x7f0c0001;
+        public static final int main=0x7f0c0002;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1817,11 +1819,15 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000d;
         public static final int action_settings=0x7f0a0012;
         public static final int app_name=0x7f0a0013;
+        public static final int button_next=0x7f0a0015;
+        public static final int button_return=0x7f0a0016;
         public static final int button_send=0x7f0a0014;
         public static final int edit_message=0x7f0a000f;
         public static final int hello_world=0x7f0a0010;
-        public static final int title_activity_display_message=0x7f0a0015;
+        public static final int title_activity_display_message=0x7f0a0018;
+        public static final int title_activity_display_next=0x7f0a0019;
         public static final int title_activity_main=0x7f0a0011;
+        public static final int title_close=0x7f0a0017;
     }
     public static final class style {
         /** 
